7920e5a5317b69743b6b0bc8f80517d78170e55d
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-06-17 13:40:55 +0530

    feat: added user schema with validations

---
+++ src/features/user/schemas/user.schema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+export const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: [3, "name should contain min of 3 characters, got {VALUE}"],
+    required: [true, "name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    unique: [true, "email already exists"],
+    validate: {
+      validator: (email) => {
+        return /[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}/.test(email);
+      },
+      message: "invalid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    minLength: [8, "min of 8 characters required, got {VALUE}"],
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["Male", "Female", "Others"],
+      message: "{VALUE} is not supported",
+    },
+  },
+});
